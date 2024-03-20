d686789ec4edb264d485e260b782f5f31bbcf397
MaxBatte <massimilian.battelli@studio.unibo.it>
2024-03-20 12:35:40 +0100

    push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 
 function App() {
   const [data, setData] = useState(null);
+  const [inputValue, setInputValue] = useState('');
 
   useEffect(() => {
     fetchData();
@@ -19,14 +20,32 @@ function App() {
     }
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3001/add-user', { // Sostituisci con l'URL corretto del tuo backend
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: inputValue }),
+      });
+      if (response.ok) {
+        fetchData();
+      }
+    } catch (error) {
+      console.error('Errore durante l\'invio dei dati:', error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-        <form method="post">
+        <form onSubmit={handleSubmit}>
           <label>Nome:  
-            <input name="myInput" value=""/>
+            <input name="myInput" value={inputValue} onChange={e => setInputValue(e.target.value)} />
           </label>
           <hr/>
           <button type="submit">Aggiungi</button>
